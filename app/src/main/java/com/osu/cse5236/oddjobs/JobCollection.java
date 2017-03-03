ec4693821df611160751c1a407ea4f6cd36cb846
Charlette Lin <zenithstorm@gmail.com>
2017-03-03 17:02:30 -0500

    Update JobCollection to use database

--- app/src/main/java/com/osu/cse5236/oddjobs/JobCollection.java
+++ app/src/main/java/com/osu/cse5236/oddjobs/JobCollection.java
@@ -1,5 +1,6 @@
 package com.osu.cse5236.oddjobs;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -7,9 +8,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import static com.osu.cse5236.oddjobs.JobDbSchema.*;
+
 public class JobCollection {
     private static JobCollection sJobCollection;
-    private List<Job> mJobs;
     private Context mContext;
     private SQLiteDatabase mDatabase;
 
@@ -23,19 +25,41 @@ public class JobCollection {
     private JobCollection(Context context) {
         mContext = context.getApplicationContext();
         mDatabase = new JobBaseHelper(mContext).getWritableDatabase();
-        mJobs = new ArrayList<>();
     }
 
+    public void addJob(Job j) {
+        ContentValues values = getContentValues(j);
+
+        mDatabase.insert(JobTable.NAME, null, values);
+    }
     public List<Job> getJobs() {
-        return mJobs;
+        return new ArrayList<>();
     }
 
     public Job getJob(UUID id) {
-        for (Job job : mJobs) {
-            if (job.getId().equals(id)) {
-                return job;
-            }
-        }
         return null;
+    }
+
+    public void updateJob(Job job) {
+        String uuidString = job.getId().toString();
+        ContentValues values = getContentValues(job);
+
+        mDatabase.update(JobTable.NAME, values,
+                JobTable.Cols.UUID + " = ?",
+                new String[] { uuidString });
+    }
+
+    private static ContentValues getContentValues(Job job) {
+        ContentValues values = new ContentValues();
+        values.put(JobTable.Cols.UUID, job.getId().toString());
+        values.put(JobTable.Cols.TITLE, job.getTitle());
+        values.put(JobTable.Cols.DESCRIPTION, job.getDescription());
+        values.put(JobTable.Cols.DATE, job.getDate().toString());
+        values.put(JobTable.Cols.PAID_HOURLY, job.isPaidHourly());
+        values.put(JobTable.Cols.COMPENSATION, job.getCompensation().toString());
+        values.put(JobTable.Cols.POSTER, job.getPoster().toString());
+        values.put(JobTable.Cols.VOLUNTEER, job.getVolunteer().toString());
+
+        return values;
     }
 }
