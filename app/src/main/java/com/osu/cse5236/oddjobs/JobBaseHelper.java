7db3de4b7f6d9c537cfee584f4598855313b2023
Charlette Lin <zenithstorm@gmail.com>
2017-03-03 16:35:16 -0500

    Add job db create method and fix job db schema

--- app/src/main/java/com/osu/cse5236/oddjobs/JobBaseHelper.java
+++ app/src/main/java/com/osu/cse5236/oddjobs/JobBaseHelper.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.osu.cse5236.oddjobs.JobDbSchema.JobTable;
+
 public class JobBaseHelper extends SQLiteOpenHelper {
     private static final int VERSION = 1;
     private static final String DATABASE_NAME = "jobBase.db";
@@ -14,6 +16,16 @@ public class JobBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + JobTable.NAME + "(" +
+                "_id integer primary key autoincrement," +
+                JobTable.Cols.UUID + ", " +
+                JobTable.Cols.TITLE + ", " +
+                JobTable.Cols.DESCRIPTION + ", " +
+                JobTable.Cols.DATE + ", " +
+                JobTable.Cols.PAID_HOURLY + ", " +
+                JobTable.Cols.COMPENSATION + ", " +
+                JobTable.Cols.POSTER + ", " +
+                JobTable.Cols.VOLUNTEER);
     }
 
     @Override
--- app/src/main/java/com/osu/cse5236/oddjobs/JobDbSchema.java
+++ app/src/main/java/com/osu/cse5236/oddjobs/JobDbSchema.java
@@ -3,16 +3,16 @@ package com.osu.cse5236.oddjobs;
 public class JobDbSchema {
     public static final class JobTable {
         public static final String NAME = "jobs";
-    }
 
-    public static final class Cols {
-        public static final String UUID = "uuid";
-        public static final String TITLE = "title";
-        public static final String DESCRIPTION = "description";
-        public static final String DATE = "date";
-        public static final String PAID_HOURLY = "paidHourly";
-        public static final String COMPENSATION = "compensation";
-        public static final String POSTER = "poster";
-        public static final String VOLUNTEER = "volunteer";
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DESCRIPTION = "description";
+            public static final String DATE = "date";
+            public static final String PAID_HOURLY = "paidHourly";
+            public static final String COMPENSATION = "compensation";
+            public static final String POSTER = "poster";
+            public static final String VOLUNTEER = "volunteer";
+        }
     }
 }
