55c15aaa9d12751f9d68609701a36bcc4978836c
Charlette Lin <zenithstorm@gmail.com>
2017-03-24 21:36:32 -0400

    Connect to online database.

--- app/src/main/java/com/osu/cse5236/oddjobs/JobFragment.java
+++ app/src/main/java/com/osu/cse5236/oddjobs/JobFragment.java
@@ -1,11 +1,11 @@
 package com.osu.cse5236.oddjobs;
 
-//import android.support.v4.Fragment;
+import android.support.v4.app.Fragment;
 
 /**
  * Created by Zenith on 3/24/2017.
  */
 
-public class JobFragment {// extends Fragment {
+public class JobFragment extends Fragment {
     // In progress; having issues with "Cannot resolve symbol 'Fragment'"
 }
