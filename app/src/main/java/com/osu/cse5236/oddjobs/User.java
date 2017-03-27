efe7e0c66f7cd7e5a86d199e2045c2f0f6f6b38f
Pcoric <pavlecoric3@gmail.com>
2017-03-27 14:09:45 -0400

    I created a UserCollection.java file, UserCursorWrapper.ava file and NewUserActivity.java file so that new users could be registered. There is also a activity_new_user.xml file for creating and registering a new user.

--- app/src/main/java/com/osu/cse5236/oddjobs/User.java
+++ app/src/main/java/com/osu/cse5236/oddjobs/User.java
@@ -16,7 +16,7 @@ public class User {
     private String mFirstName;
     private String mLastName;
     private String mEmail;
-    private ContactsContract.CommonDataKinds.Phone mPhone;
+    private String mPhone;
     private String mPassword;
     private List<Job> mPosted;
     private List<Job> mVolunteeredFor;
@@ -45,13 +45,16 @@ public class User {
         mEmail = email;
     }
 
-    public  ContactsContract.CommonDataKinds.Phone getPhone() {
+    /*public  ContactsContract.CommonDataKinds.Phone getPhone() {
         return mPhone;
-    }
+    }*/
 
-    public void setPhone( ContactsContract.CommonDataKinds.Phone phone) {
+    /*public void setPhone( ContactsContract.CommonDataKinds.Phone phone) {
         mPhone = phone;
-    }
+    }*/
+    public String getPhone() { return mPhone; }
+
+    public void setPhone(String phone) { mPhone = phone; }
 
     public String getPassword() { return mPassword; }
 
